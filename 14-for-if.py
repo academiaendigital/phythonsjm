6ef42eacaae647fc7de4a7d8def6921b950e1304
academiaendigital <130683268+academiaendigital@users.noreply.github.com>
2025-02-02 16:35:04 -0400

    clase1-clase2-sjm

---
+++ 14-for-if.py
@@ -0,0 +1,8 @@
+#for mas if
+superheroes =['batman','spiderman', 'superman']
+for superheroe in superheroes:
+    if superheroe == 'batman':
+        print('Eres el hombre de la noche')
+    else:
+        print(superheroe)
+
